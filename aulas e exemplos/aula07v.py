eb93e060c029dc170b5e947d9d51fa9ee7bd8659
Fpereirinha <pedrofpereira222@gmail.com>
2022-03-06 09:33:29 -0300

    Create aula07v.py

---
+++ aulas e exemplos/aula07v.py
@@ -0,0 +1,3 @@
+n = int(input('Qual o numero que deseja saber o antecessor e o sucessor? '))
+print('O antecessor é {} e o sucessor é {}'.format(n-1, n+1))
+print('O dobro do número é {}, o triplo é {}, e \n a raiz quadrada é {}'.format(n*2, n*3, n**(1/2)))
