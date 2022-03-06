230907fb5dad3fd398da98c669ea73006fac1134
Fpereirinha <pedrofpereira222@gmail.com>
2022-03-06 12:13:45 -0300

    Create ex63.py

---
+++ exercicios/ex63.py
@@ -0,0 +1,11 @@
+f = int(input('Quantos termos da fibo você quer ver ?'))
+t1 = 0
+t2 = 1
+print(f'{t1}\n{t2}')
+while f > 2:
+    t3 = t1 + t2
+    t1 = t2
+    t2 = t3
+    f -= 1
+    print(t3)
+print('FIM')
