ff2a28d1ea2de2e444eef9df798667c4f5aee642
Fpereirinha <pedrofpereira222@gmail.com>
2022-03-06 10:52:47 -0300

    Update ex61e62.py

--- exercicios/ex61e62.py
+++ exercicios/ex61e62.py
@@ -5,14 +5,23 @@ for c in range(t,(10*r)+t,r):
 t = int(input('Qual o primeiro termo da PA ?'))
 r = int(input('Qual a razão da PA?'))
 c = 0
+x = 10
+total = 0
 '''while c < 10:
     if c == 0:
         print(t)
     else:
         print(t + (r * c))
     c += 1'''
-while c < 10:
-    print(t)
-    c += 1
-    t += r
-str = input()
+while x != 0:
+    total += x
+    while c < total:
+
+        print(t)
+        c += 1
+        t += r
+    print('pausa')
+    x = int(input('Quantos termos a mais ?'))
+
+print('FIM total de {} números'.format(c,))
+
