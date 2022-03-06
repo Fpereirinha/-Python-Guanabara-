ea9779b836df2b5854445849f69bf9d56969662e
Fpereirinha <pedrofpereira222@gmail.com>
2022-03-06 09:47:51 -0300

    Create ex09 tabuada.py

---
+++ exercicios/ex09 tabuada.py
@@ -0,0 +1,5 @@
+n = int(input(' Qual o número para calcularmos a tabuada ? '))
+print('A tabuada do numero {}, é : {} \n {} \n {} \n {} \n {} \n {} \n {} \n {} \n {} \n {}'.format(n, n * 1, n * 2,
+                                                                                                    n * 3, n * 4, n * 5,
+                                                                                                    n * 6, n * 7, n * 8,
+                                                                                                    n * 9, n * 10))
